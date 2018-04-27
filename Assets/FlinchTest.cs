c88ab242ca1f16a09a1a825eb669aaa32bed18e5
zx29032736 <33507832+zx29032736@users.noreply.github.com>
2018-04-28 00:42:14 +0800

    Initial commit

---
+++ Assets/FlinchTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlinchTest : MonoBehaviour {
+
+    public Animator animator;
+    public CharacterController chaController;
+    bool isFlinch = false;
+    public Vector3 flinchDirection = Vector3.back;
+
+    public GameObject teageted;
+
+    private void Update()
+    {
+        if (isFlinch)
+        {
+            chaController.SimpleMove(flinchDirection * 6);
+            return;
+        }
+    }
+
+    void Flinch( Vector3 dir)
+    {
+        //moveController.isControllable = false;
+        flinchDirection = dir;
+        transform.LookAt(teageted.transform);
+        animator.Play("hurt");
+        StartCoroutine(KnockBack());
+        //moveController.isControllable = true;
+    }
+
+    IEnumerator KnockBack()
+    {
+        isFlinch = true;
+        yield return new WaitForSeconds(0.2f);
+        isFlinch = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# ActionRpg_Repository
